f080c67bbffca58e81c4288dbb7f6a6dffac1b7b
이지은/고객파트너개발팀 <jieun2.lee@musinsa.com>
2024-05-25 12:36:07 +0900

    Initial commit

---
+++ src/test/java/study/timo/springsecurityinaction/SpringSecurityInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package study.timo.springsecurityinaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityInActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
