2796c3b8998c0d93e925e10afe44152dd49eeecd
이지은/고객파트너개발팀 <jieun2.lee@musinsa.com>
2024-06-02 16:21:17 +0900

    [2장. 안녕! 스프링 시큐리티] 예제 2.18 권한 부여 관리를 위한 구성 클래스 정의

---
+++ src/main/java/study/timo/springsecurityinaction/config/WebAuthorizationConfig.java
@@ -0,0 +1,20 @@
+package study.timo.springsecurityinaction.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class WebAuthorizationConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.httpBasic(withDefaults());
+        http.authorizeHttpRequests(auth ->
+                auth.anyRequest().authenticated());
+        return http.build();
+    }
+}
