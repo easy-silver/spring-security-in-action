2fde13ca1e5de699a9b2870d179248a975780878
이지은/고객파트너개발팀 <jieun2.lee@musinsa.com>
2024-06-02 15:25:50 +0900

    [2장. 안녕! 스프링 시큐리티] 예제 2.9 UserDetailsService 재구성

--- src/main/java/study/timo/springsecurityinaction/config/ProjectConfig.java
+++ src/main/java/study/timo/springsecurityinaction/config/ProjectConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -15,28 +15,26 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 public class ProjectConfig {
     @Bean
-    public UserDetailsService userDetailsService() {
-        var userDetailsService = new InMemoryUserDetailsManager();
-        var user = User.withUsername("john")
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.httpBasic(withDefaults());
+        http.authorizeHttpRequests(auth ->
+                auth.anyRequest().authenticated()
+        );
+        return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User
+                .withUsername("john")
                 .password("12345")
                 .authorities("read")
                 .build();
-
-        userDetailsService.createUser(user);
-        return userDetailsService;
+        return new InMemoryUserDetailsManager(user);
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.httpBasic(withDefaults());
-        http.authorizeHttpRequests(auth ->
-                auth.anyRequest().permitAll()
-        );
-        return http.build();
     }
 }
