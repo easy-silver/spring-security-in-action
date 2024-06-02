9c3c4a9a7cade5b950a787e72791a2a89162b3ab
이지은/고객파트너개발팀 <jieun2.lee@musinsa.com>
2024-06-02 15:10:32 +0900

    [2장. 안녕! 스프링 시큐리티] 예제 2.7 모든 요청에 인증 설정

--- src/main/java/study/timo/springsecurityinaction/config/ProjectConfig.java
+++ src/main/java/study/timo/springsecurityinaction/config/ProjectConfig.java
@@ -2,11 +2,15 @@ package study.timo.springsecurityinaction.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class ProjectConfig {
@@ -25,5 +29,14 @@ public class ProjectConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.httpBasic(withDefaults());
+        http.authorizeHttpRequests(auth ->
+                auth.anyRequest().authenticated()
+        );
+        return http.build();
     }
 }
