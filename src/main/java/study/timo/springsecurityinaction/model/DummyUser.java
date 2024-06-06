b3997ccc34fef9bc9d4bbd796de58f56f85cd720
이지은/고객파트너개발팀 <jieun2.lee@musinsa.com>
2024-06-06 10:55:06 +0900

    [3장. 사용자 관리] 예제 3.2 최소한의 UserDetails 구현 작성

---
+++ src/main/java/study/timo/springsecurityinaction/model/DummyUser.java
@@ -0,0 +1,39 @@
+package study.timo.springsecurityinaction.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class DummyUser implements UserDetails {
+    @Override
+    public String getUsername() {
+        return "bill";
+    }
+
+    @Override
+    public String getPassword() {
+        return "12345";
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(() -> "READ");
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+}
