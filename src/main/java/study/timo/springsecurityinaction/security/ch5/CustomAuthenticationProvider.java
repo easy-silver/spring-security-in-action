3a3c110ed5370c6728a7dc1b235540da9e8a12bf
이지은/커뮤니티개발팀 <jieun2.lee@musinsa.com>
2024-06-12 20:27:10 +0900

    [5장. 인증 구현] 예제 5.3 AuthenticationProvider의 supports() 메서드 재정의

---
+++ src/main/java/study/timo/springsecurityinaction/security/ch5/CustomAuthenticationProvider.java
@@ -0,0 +1,21 @@
+package study.timo.springsecurityinaction.security.ch5;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
